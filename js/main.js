069eff7bf95a8811c399e7e22d7b7834b2a2fe5c
Nadernada <nadernada604@gmail.com>
2024-12-20 16:14:16 +0100

    New modifs

--- js/main.js
+++ js/main.js
@@ -58,6 +58,27 @@
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:6
+            },
+            768:{
+                items:6
+            }
+        }
+    });
+    // presentation carousel
+    $(".presentation-carousel").owlCarousel({
         autoplay: false,
         smartSpeed: 1000,
         center: true,
